46b0cd6a47f35bb928fd73879104c4708a24ba9c
majung2 <suelmj1417@gmail.com>
2019-06-09 13:33:23 +0000

    boot

--- mijeong_0321/urls.py
+++ mijeong_0321/urls.py
@@ -4,7 +4,7 @@ import wordcount.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/',wordcount.views.home, name='home'),
+    url(r'^$',wordcount.views.home, name='home'),
     url(r'^result/',wordcount.views.result, name='result'),
 
 ]
